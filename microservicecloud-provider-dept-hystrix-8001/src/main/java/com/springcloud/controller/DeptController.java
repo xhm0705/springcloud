142e29ca5702087c0944fb3562425a2ddc298881
xhm865696761@163.com <865696761@qq.com>
2020-04-29 14:00:20 +0800

    first commit

---
+++ microservicecloud-provider-dept-hystrix-8001/src/main/java/com/springcloud/controller/DeptController.java
@@ -0,0 +1,29 @@
+package com.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+@RestController
+public class DeptController
+{
+
+	@RequestMapping(value = "/dept/get", method = RequestMethod.GET)
+	//一旦调用服务方法失败并抛出了错误信息后，会自动调用@HystrixCommand标注好的fallbackMethod调用类中的指定方法
+	@HystrixCommand(fallbackMethod = "processHystrix_Get")
+	public String get()
+	{
+
+		return "";
+
+	}
+
+	public String processHystrix_Get()
+	{
+		return new String("该ID：没有没有对应的信息,null--@HystrixCommand");
+	}
+}
